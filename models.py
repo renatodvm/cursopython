de755a292933d0d450f9605ee5a9b2a3c2f141ac
renatodvm <renatodvm@gmail.com>
2017-01-26 17:52:52 -0200

    Aula 10 do curso de Python

--- models.py
+++ models.py
@@ -7,9 +7,16 @@ class Perfil(object):
         self.nome = nome
         self.telefone = telefone
         self.empresa = empresa
+        self.__curtidas = 0
 
     def imprimir(self):
-        print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
+        print 'Nome: %s, Telefone: %s, Empresa: %s, Curtidas %s' % (self.nome, self.telefone, self.empresa, self.__curtidas)
+
+    def curtir(self):
+        self.__curtidas+=1
+
+    def obter_curtidas(self):
+        return self.__curtidas
 
 
 class Data(object):
